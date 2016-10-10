ff59734ca2ed1ab97c797c8d0f40fec036f2f3fa
Кирилл <hi.sender@gmail.com>
2016-10-10 18:48:36 +0300

    mina 1.0

--- lib/mina/unicorn/utility.rb
+++ lib/mina/unicorn/utility.rb
@@ -93,7 +93,7 @@ module Mina
         %Q%
           #{duplicate_unicorn}
 
-          sleep #{unicorn_restart_sleep_time}; # in order to wait for the (old) pidfile to show up
+          sleep #{fetch(:unicorn_restart_sleep_time)}; # in order to wait for the (old) pidfile to show up
 
           if #{old_unicorn_is_running?}; then
             #{unicorn_send_signal('QUIT', get_old_unicorn_pid)};
